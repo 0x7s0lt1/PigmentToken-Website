ca7450da284f53e522aa735baaba020d0d484d6b
Fehérvári Zsolt <f.zsolt@niarz.com>
2023-12-28 02:02:55 +0100

    pscan link

--- src/common/layout/footer/Footer.tsx
+++ src/common/layout/footer/Footer.tsx
@@ -1,11 +1,14 @@
 import {FC} from "react";
+import Link from "next/link";
 
 const Footer: FC = () => {
     return (
         <footer className="footer container shadow">
             <div className="footer-inner shadow">
                 <div className="d-flex align-items-center flex-wrap justify-content-center">
-                    <img src='/img/p-logo.png'/>
+                    <Link target="_blank" href={"https://polygonscan.com/token/0xc76440dbdb9b42ae2bf0269d12280614604d43e8"}>
+                        <img alt='PolygonScan' src='/img/p-scan.svg'/>
+                    </Link>
                     <p className="text-white">© 2023 Pigment Token. All rights reserved.</p>
                 </div>
             </div>
